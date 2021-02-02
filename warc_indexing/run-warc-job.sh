8aa328cf32e95a368d08504a61569c1bddcaf031
Andrew Jackson <Andrew.Jackson@bl.uk>
2021-02-02 16:06:50 +0000

    Switched warc_job to mapper_raw.

--- warc_indexing/run-warc-job.sh
+++ warc_indexing/run-warc-job.sh
@@ -2,5 +2,5 @@
 
 export MRJOB_CONF=${PWD}/mrjob.conf
 
-python warc_job.py --archives '../venv.zip#venv' -r hadoop -o output.warced "hdfs:///0_original/fc/opera/heritrix/output/warcs/daily-0600/20150812050010/BL-20150812050021920-00000-25859~opera~8443.warc.gz"
+python warc_job.py --archives '../venv.zip#venv' -r hadoop "hdfs:///0_original/fc/opera/heritrix/output/warcs/daily-0600/20150812050010/BL-20150812050021920-00000-25859~opera~8443.warc.gz"
 
