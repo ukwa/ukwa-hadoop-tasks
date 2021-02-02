69fc414952914abaaee9d64397b72402ad708fc0
Andrew Jackson <Andrew.Jackson@bl.uk>
2021-02-02 14:47:22 +0000

    Working CDX index core, runs local too.

--- ukwa/cdx_job.py
+++ ukwa/cdx_job.py
@@ -1,8 +1,24 @@
+from datetime import datetime
 from mrjob.job import MRJob
-from warcio.archiveiterator import ArchiveIterator
+from urllib.parse import urlparse
 
-class MRCDXIndexerCP(MRJob):
+class MRCDXIndexer(MRJob):
 
+    def jobconf(self):
+        return {
+            'mapred.job.name': '%s_%s' % ( self.__class__.__name__, datetime.now().isoformat() ),
+            'mapred.compress.map.output':'true',
+            'mapred.output.compress': 'true',
+            'mapred.output.compression.codec': 'org.apache.hadoop.io.compress.GzipCodec',
+        }
+
+    # Sort all the values on each Reducer, so the last event wins in the case of timestamp collisions:`
+    SORT_VALUES = True
+
+    # Using mapper_raw means MrJob arranges for a copy of each WARC to be placed where we can get to it:
+    # (This breaks data locality, but streaming through large files is not performant because they get read into memory)
+    # (A FileInputFormat that could reliably split block GZip files would be the only workable fix)
+    # (But TBH this is pretty fast as it is)
     def mapper_raw(self, warc_path, warc_uri):
         from cdxj_indexer.main import CDX11Indexer
 
@@ -11,88 +27,25 @@ class MRCDXIndexerCP(MRJob):
         cdx11 = CDX11Indexer(inputs=[warc_path], output=cdx_file, cdx11=True, post_append=True)
         cdx11.process_all()
 
-        self.set_status('Processed.... Now reading....')
+        warc_path = urlparse(warc_uri).path
+
+        self.set_status('cdxj_indexer of %s complete.' % warc_path)
 
         with open(cdx_file) as f:
             for line in f:
-                self.increment_counter('cdx', 'cdx_lines', 1)
-                yield warc_uri, line.strip()
-
-    def reducer(self, key, values):
-        for value in values:
-            yield key, value
-
-
-
-class MRCDXIndexerStream(MRJob):
-
-    #
-    # Using this custom input format, rather than 'mapred.min.split.size' as
-    # The default TextInputFormat auto-gunzips and leaves only the first record.
-    #
-    HADOOP_INPUT_FORMAT = 'uk.bl.wa.hadoop.mapred.UnsplittableInputFileFormat'
-
-    LIBJARS = [ "../jars/warc-hadoop-recordreaders-3.2.0-SNAPSHOT-job.jar" ]
-
-    SORT_VALUES = True
-
-    JOBCONF = {
-        'mapred.job.name': 'MRCDXIndexerStreamJob',
-    } 
-
-    def configure_options(self):
-        super(CCJob, self).configure_options()
-        # Pass through the runner so we can adapt:
-        self.pass_through_option('--runner')
-        self.pass_through_option('-r')
-
-    def _wrap_protocols(self, step_num, step_type):
-        '''Override the default implementation to directly parse stdin as a WARC.'''
-        read_lines, write_line = super()._wrap_protocols(step_num, step_type)
-
-        def read_records():
-            from cdxj_indexer.main import CDX11Indexer
-
-            paths = self.options.args or ['-']
-            
-            self.set_status( 'CHECK read_records %s' % paths)
-
-            for path in paths:
-                if path == '-':
-                    # On Hadoop, read up to the first \t to get the path (passed in by UnsplittableInputFileFormat):
-                    if self.options.runner == 'inline':
-                        key = path
-                    else:
-                        key = ""
-                        while True:
-                            char = self.stdin.read(1)
-                            if char == b'\t':
-                                break
-                            key += char.decode("utf-8")
-
-                    # Then index the file by reading stdin:
-                    cdx_file = 'index.cdx'
-                    cdx11 = CDX11Indexer(inputs='-', output=cdx_file, cdx11=True, post_append=True)
-                    cdx11.process_all()
-
-                    self.set_status('Processed.... Now reading....')
-
-                    with open(cdx_file) as f:
-                        for line in f:
-                            self.increment_counter('cdx', 'cdx_lines', 1)
-                            yield key, line.strip()
-                else:
-                    with open(path, 'rb') as stream:
-                        pass
-
-        # If this is a mapper, parse to records:
-        if step_type == 'mapper':
-            return read_records, write_line
-        else:
-            return read_lines, write_line
-
-    def mapper(self, key, value):
-        yield key, value
+                line = line.strip()
+                parts = line.split(" ")
+                # Skip header:
+                if parts[0] == 'CDX':
+                    continue
+                # Count the lines:
+                self.increment_counter('CDX', 'CDX_LINES', 1)
+                # Replace `warc_path` with proper HDFS path:
+                parts[10] = warc_path
+                # Key on host to distribute load:
+                host_surt = parts[0].split(")", 1)[0]
+                # Reconstruct the CDX line and yield:
+                yield host_surt, " ".join(parts)
 
     def reducer(self, key, values):
         for value in values:
@@ -101,6 +54,5 @@ class MRCDXIndexerStream(MRJob):
 
 
 if __name__ == '__main__':
-    MRCDXIndexerCP.run()
-    #MRCDXIndexerStream.run()
+    MRCDXIndexer.run()
 
