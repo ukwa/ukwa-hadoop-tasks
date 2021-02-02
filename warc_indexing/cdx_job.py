c895fe959f345d89bc686c464bda35e0f445303f
Andrew Jackson <Andrew.Jackson@bl.uk>
2021-02-02 15:54:11 +0000

    Added working CDX indexer.

--- warc_indexing/cdx_job.py
+++ warc_indexing/cdx_job.py
@@ -1,8 +1,23 @@
-from datetime import datetime
+import logging
+import requests
 from mrjob.job import MRJob
+from datetime import datetime
 from urllib.parse import urlparse
 
+
+logger = logging.getLogger(__name__)
+
+
 class MRCDXIndexer(MRJob):
+
+    def configure_args(self):
+        super().configure_args()
+        self.add_passthru_arg(
+            '-R', '--num-reducers', default=5,
+            help="Number of reducers to use.")
+        self.add_passthru_arg(
+            '-C', '--cdx-endpoint', required=True,
+            help="CDX service endpoint to use, e.g. 'http://server/collection'.")
 
     def jobconf(self):
         return {
@@ -10,6 +25,8 @@ class MRCDXIndexer(MRJob):
             'mapred.compress.map.output':'true',
             'mapred.output.compress': 'true',
             'mapred.output.compression.codec': 'org.apache.hadoop.io.compress.GzipCodec',
+            'mapred.reduce.tasks': str(self.options.num_reducers),
+            'mapreduce.job.reduces': str(self.options.num_reducers)
         }
 
     # Sort all the values on each Reducer, so the last event wins in the case of timestamp collisions:`
@@ -47,9 +64,47 @@ class MRCDXIndexer(MRJob):
                 # Reconstruct the CDX line and yield:
                 yield host_surt, " ".join(parts)
 
+    def reducer_init(self):
+        self.ocdx = OutbackCDXClient(self.options.cdx_endpoint)
+
     def reducer(self, key, values):
+        counter = 0
         for value in values:
-            yield key, value
+            counter += 1
+            # Send to OutbackCDX:
+            self.ocdx.add(value)
+
+        # Also emit some stats from the job:
+        yield key, counter
+
+    def reducer_final(self):
+        self.ocdx.send()
+
+
+class OutbackCDXClient():
+
+    def __init__(self, cdx_server, buf_max=1000):
+          self.cdx_server = cdx_server
+          self.buf_max = buf_max
+          self.postbuffer = []
+          self.session = requests.Session()
+    
+    def send(self):
+        chunk = "\n".join(self.postbuffer)
+        r = self.session.post(self.cdx_server, data=chunk.encode('utf-8'))
+        if (r.status_code == 200):
+            self.postbuffer = []
+            logger.info("POSTed to cdxserver: %s" % self.cdx_server)
+            return
+        else:
+            logger.error("Failed with %s %s\n%s" % (r.status_code, r.reason, r.text))
+            logger.error("Failed submission was: %s" % chunk.encode('utf-8'))
+            raise Exception("Failed with %s %s\n%s" % (r.status_code, r.reason, r.text))
+ 
+    def add(self, cdx11_line):
+        self.postbuffer.append(cdx11_line)
+        if len(self.postbuffer) > self.buf_max:
+            self.send()
 
 
 
