68b83ed74ea19e8b7bc03e525e1f4e49718293df
Andrew Jackson <Andrew.Jackson@bl.uk>
2021-02-02 14:55:46 +0000

    Better folder name.

--- ukwa/run-cdx-job-local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export MRJOB_CONF=${PWD}/mrjob.conf
-
-python cdx_job.py -r local -o output.cdx "BL-20150812050021920-00000-25859~opera~8443.warc.gz" "BL-NPLD-WEBRENDER-frequent-npld-20210119131039-20210201012219321-02844-n4o6ljbu.warc.gz"
-
--- ukwa/run-warc-job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export MRJOB_CONF=${PWD}/mrjob.conf
-
-python warc_job.py --archives '../cdxenv.zip#venv' --read-logs --jobconf mapred.job.name=MRWarcJob -r hadoop -o output.warced "hdfs:///0_original/fc/opera/heritrix/output/warcs/daily-0600/20150812050010/BL-20150812050021920-00000-25859~opera~8443.warc.gz"
-
